2d395122e1c5ce0b6687c0e1088f4e1035b9e5f1
ericabasak <ericabasak@gmail.com>
2019-12-10 15:47:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+let dodger = document.querySelector("#dodger");
+
+// move to the LEFT
+function moveDodgerLeft(){
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0 ) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// move to the RIGHT
+function moveDodgerRight(){
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+// for the right move
+// document.addEventListener("keydown", 
+// function(e){
+//   if (e.key === "ArrowRight"){
+//     moveDodgerRight();
+//   } 
+//   });
+
+// // for the left move
+// document.addEventListener("keydown", 
+// function(e){
+//   if (e.key === "ArrowLeft"){
+//     moveDodgerLeft();
+//   } 
+//   });
+
+document.addEventListener("keydown", 
+  function(e){
+    switch(e.key){
+      case "ArrowLeft":
+          moveDodgerLeft();
+        break;
+      case "ArrowRight":
+          moveDodgerRight();
+        break;
+    }
+  })
